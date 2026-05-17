7e36faf72657d8641ac2d5ae71e7c761c9783751
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-17 20:12:33 +0700

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,68 @@
+const levels = [
+  {
+    playerSpawn: {
+      x: 220,
+      y: 300
+    },
+
+    goal: {
+      x: 700,
+      y: 380,
+      width: 40,
+      height: 40
+    },
+
+    platforms: [
+      {
+        x: 160,
+        y: 480,
+        width: 640,
+        height: 60
+      },
+
+      {
+        x: 420,
+        y: 380,
+        width: 120,
+        height: 20
+      }
+    ]
+  },
+
+  {
+    playerSpawn: {
+      x: 220,
+      y: 300
+    },
+
+    goal: {
+      x: 600,
+      y: 260,
+      width: 40,
+      height: 40
+    },
+
+    platforms: [
+      {
+        x: 160,
+        y: 480,
+        width: 640,
+        height: 60
+      },
+
+      {
+        x: 300,
+        y: 400,
+        width: 100,
+        height: 20
+      },
+
+      {
+        x: 500,
+        y: 320,
+        width: 100,
+        height: 20
+      }
+    ]
+  }
+];

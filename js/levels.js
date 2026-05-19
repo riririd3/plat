5ea2de39b038b2ed88943a48ed18aa61958bc6f0
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-19 22:06:20 +0700

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -2,23 +2,23 @@
 export const LEVEL_MAPS = [
     {
         // Level 1
-        playerSpawn: { x: 40, y: 480 }, // Start near ground level
+        playerSpawn: { x: 40, y: 460 },
         platforms: [
-            { x: 0, y: 500, w: 200, h: 20 },    // Ground/platform at bottom
-            { x: 250, y: 450, w: 100, h: 20 },   // Mid platform
-            { x: 400, y: 380, w: 100, h: 20 },   // Higher platform
-            { x: 550, y: 300, w: 100, h: 20 }    // Highest platform with star
+            { x: 0, y: 500, w: 200, h: 20 },
+            { x: 250, y: 450, w: 100, h: 20 },
+            { x: 400, y: 380, w: 100, h: 20 },
+            { x: 550, y: 300, w: 100, h: 20 }
         ],
         spikes: [
-            { x: 300, y: 490, w: 40, h: 10 }     // Spike on ground level
+            { x: 300, y: 490, w: 40, h: 10 }
         ],
         stars: [
-            { x: 600, y: 270, w: 20, h: 20 }     // Star on highest platform
+            { x: 600, y: 270, w: 20, h: 20 }
         ]
     },
     {
         // Level 2
-        playerSpawn: { x: 40, y: 480 },
+        playerSpawn: { x: 40, y: 460 },
         platforms: [
             { x: 0, y: 500, w: 150, h: 20 },
             { x: 200, y: 430, w: 80, h: 20 },
@@ -28,7 +28,7 @@ export const LEVEL_MAPS = [
         ],
         spikes: [
             { x: 150, y: 490, w: 30, h: 10 },
-            { x: 400, y: 280, w: 40, h: 10 }     // Spike on platform
+            { x: 400, y: 280, w: 40, h: 10 }
         ],
         stars: [
             { x: 650, y: 190, w: 20, h: 20 }

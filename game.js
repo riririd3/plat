04772ff6d07a75c33fd55846551858509cc5f4c2
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-23 01:39:54 +0700

    Update game.js

--- game.js
+++ game.js
@@ -16,9 +16,6 @@ const BASE_HEIGHT = 540;
 // 🔊 Centralized Sound Manager
 function playSound(type) {
   if (isMuted) return;
-    if (!z_ctx) z_ctx = new (window.AudioContext || window.webkitAudioContext)();
-    if (z_ctx.state === 'suspended') z_ctx.resume();
-  }
     if (type === 'jump') zzfx(...[.5,,458,.05,.03,.07,,3,,198,,,,,,,.04,.53,.03,,-1462]); // Jump 3
     if (type === 'gravity') zzfx(...[.5,,286,.01,.03,.38,2,.43,-8.1,-0.1,-50,-0.01,.02,.2,,,.01,1.09,.05,.01]); // Random 38
     if (type === 'spike') zzfx(...[.8,,80,,.05,.2,1,0,,,,, .1]);

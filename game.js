0da3949e0d362899898ed4428db8e073671a3fc1
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-24 19:17:28 +0700

    Update game.js

--- game.js
+++ game.js
@@ -413,8 +413,7 @@ function handleTouch(e) {
 
         if (x > startX && x < startX + startMenuBtn.w && y > startY && y < startY + startMenuBtn.h) {
           if (!musicPlayer) {
-            musicPlayer = new ZzFXM(...song);
-            musicPlayer.play();
+            musicPlayer = zzfxP(...new ZzFXM(...song));
           }
           currentLevelIndex = 0; totalPlayTime = 0.0; loadLevel(currentLevelIndex); return;
         }

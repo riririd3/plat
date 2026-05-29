ac9604c19015e19134718b389189dd94beafcdc7
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-29 21:16:12 +0700

    Update game.js

--- game.js
+++ game.js
@@ -83,7 +83,7 @@ let starPulseTime = 0;
 let touch = { left: false, right: false, jump: false };
 const dpad = { size: 50 }, jumpBtn = { size: 50 }, restartBtn = { size: 40 };
 const startMenuBtn = { w: 200, h: 50 }, centerFullBtn = { w: 200, h: 45 };
-const midX = GAME_X() + GAME_WIDTH() / 2;
+
 const btnW = 200;
 const btnH = 50;
 
@@ -314,6 +314,7 @@ function drawRestartButton() {
 }
 
 function drawMenuButtons() {
+  const midX = GAME_X() + GAME_WIDTH() / 2;
   if (menuState === "main") {
     // START BUTTON
     context.fillStyle = "#10b981";
